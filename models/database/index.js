88d6f784c0b045f3c5592d3e47991437e0c675ae
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-07-07 00:48:43 -0300

    refactor: Atualização do index.js do connection.
    Foi acrescentado uma linha no index.js do connection, para deixar o número da porta que está sendo usada, para caso alguém utilize uma porta diferente da padrão que é 3306 para o MySQL.

--- models/database/index.js
+++ models/database/index.js
@@ -6,6 +6,7 @@ const connection = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 
 module.exports = connection;
